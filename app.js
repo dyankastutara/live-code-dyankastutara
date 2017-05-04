7d6e4d29d405516ab31dc862779beec9293e7b6c
dyankastutara <dyankastutara19@gmail.com>
2017-05-04 10:53:56 +0700

    add models

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose')
+var foods = require('./routes/foods');
+var restaurants = require('./routes/restaurants');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/live-coding');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Connected");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +30,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/foods', foods);
+app.use('/restaurants', restaurants);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
